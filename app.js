d482f50e077bdc88c64a59d3a29f1184f72b53fc
Helder Carmen <heldercarmen@gmail.com>
2020-04-06 12:30:05 +0100

    app.js now reads and writes to json

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const PORT = process.env.PORT || 4001;
 const pug = require('pug')
 const path = require('path');
 const app= express();
+const fs= require('fs');
+
+app.set('view engine', 'pug');
 
 app.use(express.urlencoded({extended: false}));
 
@@ -11,6 +14,40 @@ app.use(express.static(path.join(__dirname,'views')));
 app.use(express.static(path.join(__dirname,'uploads')));
 
 const compiledHomepPageTemplate = pug.compileFile('views/home.pug');
+
+
+function savePost(postData){
+
+    // let post = { 
+    //     id: null, 
+    //     title: postData.title,
+    //     message: postData.message,
+    //     comments: {},
+    //     emoji: null,
+    //     giphy: null    
+    
+    // };
+
+    let post = { 
+        descriptionShort: postData.descriptionShort,
+        condition: postData.condition,
+        price: postData.price,
+        details: postData.details,
+    };
+
+    var fs = require('fs')
+
+    fs.readFile('./posts.json', 'utf-8', function(err, data) {
+        if (err) throw err
+        var arrayOfObjects = JSON.parse(data)
+        arrayOfObjects.posts.push(post)
+    
+        fs.writeFile('./posts.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+            if (err) throw err
+            console.log('Done!')
+        })
+    })
+}
 
 
 app.get("/", (req,res) => {
@@ -23,6 +60,14 @@ app.get("/", (req,res) => {
     })
     
     res.send(homepage) 
+})
+
+app.post("/submitPost", (req,res) => {
+    
+    console.log("data recieved", req.body)
+    savePost(req.body)
+    res.send("sucessfully saved post")
+
 })
 
 app.listen(PORT);
