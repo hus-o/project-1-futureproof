7dc75cbbad95f8d73491ed4fb991eab0e0031a3a
Helder Carmen <heldercarmen@gmail.com>
2020-04-06 10:52:30 +0100

    add pug template and change app.js to use pug

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 4001;
+const pug = require('pug')
+const fs = require('fs');
+
+const app= express();
