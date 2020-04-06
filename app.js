27f714f86eb0535625131d471fb519ee6f14f340
liam505 <61749964+liam505@users.noreply.github.com>
2020-04-06 12:55:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,20 +3,51 @@ const PORT = process.env.PORT || 4001;
 const pug = require('pug')
 const path = require('path');
 const app= express();
-const fs = require("fs")
+
+const fs = require('fs');
+
+// let blog = {
+//     ID: 1,
+//     userName: "text", 
+//     postContent: "text",
+//     comments: {},
+//     gif: "giphy1",
+//     emoji: "smiley"
+// };
+// let data = JSON.stringify(blog)
+
+// fs.writeFileSync('db.json', data, (err) => {
+//     if(err){
+//         console.log('success')
+//     }
+// })
+
+function updatePostDb(blog){
+	
+	let data = JSON.stringify(blog)
+
+	fs.writeFile('db.json', data, (err) => {
+		if(err){
+			console.log('success')
+		}
+	})
+}
+
+
+
 
 app.use(express.urlencoded({extended: false}));
+
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,'views')));
 app.use(express.static(path.join(__dirname,'uploads')));
-app.set('view engine', 'pug')
 
-const compiledHomePageTemplate = pug.compileFile('views/home.pug');
+const compiledHomepPageTemplate = pug.compileFile('views/home.pug');
 
 
 app.get("/", (req,res) => {
           
-    let homepage = compiledHomePageTemplate({
+    let homepage = compiledHomepPageTemplate({
         descriptionShort: "",
         condition: "",
         price:"",
@@ -26,13 +57,15 @@ app.get("/", (req,res) => {
     res.send(homepage) 
 })
 
-app.get("/blogposts", (req,res) =>{
-    let rawData = fs.readFileSync("db.json")
-    let blogData = JSON.parse(rawData)
-    console.log(blogData)
-    res.render("results",{blogData:blogData})
-    
+app.post("/submitPost", (req, res) => {
+    const uName = req.body.userName;
+    const blogContent = req.body.postContent;
+    updatePostDb(uName);
+    updatePostDb(blogContent)
 })
+
+
+
 
 app.listen(PORT);
 console.log("server is listening");
