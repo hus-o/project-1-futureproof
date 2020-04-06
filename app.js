4fe622dd827822b1de883420d6466cf7de2f4880
fatima03598 <54324041+fatima03598@users.noreply.github.com>
2020-04-06 12:51:56 +0100

    something random

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 4001;
 const pug = require('pug')
 const path = require('path');
+const fs = require('fs');
 const app= express();
 
 app.use(express.urlencoded({extended: false}));
@@ -16,14 +17,38 @@ const compiledHomepPageTemplate = pug.compileFile('views/home.pug');
 app.get("/", (req,res) => {
           
     let homepage = compiledHomepPageTemplate({
-        descriptionShort: "",
-        condition: "",
-        price:"",
+        descriptionShort: "",      
         details:"",
     })
     
     res.send(homepage) 
 })
+
+app.post("/submission",(req,res,next) => {
+    log = req.body;
+    console.log(log);
+    res.send(log)
+    fs.readFile('./data.json', 'utf-8', function(err, data) {
+        if (err) throw err
+    
+        var dataObject = JSON.parse(data)
+        dataObject.push({log})
+        
+  
+    // fs.writeFile('data.json', dataString, (err) => {
+    //     if (err) throw err;
+    //     console.log('Data written to file');
+    // });
+    
+    // console.log('This is after the write call');
+    
+        fs.writeFile('./data.json', JSON.stringify(dataObject), 'utf-8', function(err) {
+            if (err) throw err
+            console.log('Done!')
+        })
+    })
+});
+
 
 app.listen(PORT);
 console.log("server is listening");
