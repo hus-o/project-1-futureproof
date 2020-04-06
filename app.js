15ec28b72ac89d386e71b3c7521420528c1a841d
huseyin <thehusko@gmail.com>
2020-04-06 12:48:53 +0100

    iterative output of JSON data to page

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.use(express.static(path.join(__dirname,'uploads')));
 app.set('view engine', 'pug')
 
 const compiledHomePageTemplate = pug.compileFile('views/home.pug');
-const compiledPostsPageTemplate = pug.compileFile("views/results.pug")
-let rawData = fs.readFileSync("db.json")
-let name = JSON.parse(rawData)
-console.log(name[0].userName)
 
 
 app.get("/", (req,res) => {
@@ -31,14 +27,10 @@ app.get("/", (req,res) => {
 })
 
 app.get("/blogposts", (req,res) =>{
-    /* let rawData = fs.readFileSync("db.json")
-    name = JSON.parse(rawData)
-    console.log(name) */
-   /*  let blogPage = compiledPostsPageTemplate({
-        user:
-        content:
-        gif:
-    }) */
+    let rawData = fs.readFileSync("db.json")
+    let blogData = JSON.parse(rawData)
+    console.log(blogData)
+    res.render("results",{blogData:blogData})
     
 })
 
