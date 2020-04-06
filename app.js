c5290a890e02dd0448923a2fad707bc98e77927a
huseyin <thehusko@gmail.com>
2020-04-06 12:28:45 +0100

    able to read from JSON, now make it iterable

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ const PORT = process.env.PORT || 4001;
 const pug = require('pug')
 const path = require('path');
 const app= express();
+const fs = require("fs")
 
 app.use(express.urlencoded({extended: false}));
-
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static(path.join(__dirname,'views')));
 app.use(express.static(path.join(__dirname,'uploads')));
+app.set('view engine', 'pug')
 
-const compiledHomepPageTemplate = pug.compileFile('views/home.pug');
+const compiledHomePageTemplate = pug.compileFile('views/home.pug');
+const compiledPostsPageTemplate = pug.compileFile("views/results.pug")
+let rawData = fs.readFileSync("db.json")
+let name = JSON.parse(rawData)
+console.log(name[0].userName)
 
 
 app.get("/", (req,res) => {
           
-    let homepage = compiledHomepPageTemplate({
+    let homepage = compiledHomePageTemplate({
         descriptionShort: "",
         condition: "",
         price:"",
@@ -23,6 +28,18 @@ app.get("/", (req,res) => {
     })
     
     res.send(homepage) 
+})
+
+app.get("/blogposts", (req,res) =>{
+    /* let rawData = fs.readFileSync("db.json")
+    name = JSON.parse(rawData)
+    console.log(name) */
+   /*  let blogPage = compiledPostsPageTemplate({
+        user:
+        content:
+        gif:
+    }) */
+    
 })
 
 app.listen(PORT);
