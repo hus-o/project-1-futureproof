9019de1d54f51bfe720112ba02e88daa10b3b1fc
Helder Carmen <heldercarmen@gmail.com>
2020-04-07 11:44:11 +0100

    working hide button for comments

--- public/client.js
+++ public/client.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(){
+window.addEventListener('DOMContentLoaded', (event) => {
+    
     $("#gifSearch").click(event => {
         event.preventDefault();
         let userQuery = $("#gif").val();
@@ -19,17 +20,56 @@ document.addEventListener("DOMContentLoaded", function(){
 
     function setToHidden(nodeLists){
         let ids=nodeLists[0]
-    
         ids.forEach(element => {
             if(element.type !== "file"){
                 element.hidden=true;
             };
         })
-      
     }
     
     var idInputs=document.getElementsByName("ID")
-    
-    
     setToHidden([idInputs])
+
+
+function addIdToButton(nodeLists){
+    var list = document.getElementsByClassName("toggleComments");
+    let i=0;
+    for (let item of list) {
+        let att = document.createAttribute("id");   
+        att.value = i++
+        item.setAttributeNode(att)
+    }
+}
+
+addIdToButton([document.getElementsByClassName("toggleComments")])
+
+
+    function toggleComments(n){
+        
+        let x=$(".comments")[n];
+        
+        console.log($(x).css('display'))
+        
+        if($(x).css('display') === 'none') {
+            $(x).css('display', "block");
+        }
+        else if($(x).css('display')=== "block"){
+            $(x).css('display', "none");
+         }
+    }
+
+    $("button").click(function() { 
+        var t = $(this).attr('id'); 
+
+        toggleComments(t)
+
+        if($(this).text()==="Show Comments"){
+            $(this).text("Hide Comments")
+        }
+        else if($(this).text()==="Hide Comments"){
+            $(this).text("Show Comments")
+        }
+    
+    });  
+
 })
