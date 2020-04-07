cc63edb8d65d167cb85ce068378a076a38885812
huseyin <thehusko@gmail.com>
2020-04-07 12:48:39 +0100

    showing 5 gifs based on search term

--- public/client.js
+++ public/client.js
@@ -6,14 +6,16 @@ document.addEventListener("DOMContentLoaded", function(){
         console.log(userQuery)
     })
 
-
     function getGIF(userQuery){
         const key = "6X4aryqB9MRq0HmQ80Eh3GBw22RcLCx6";
-        /* let usedQuery = query.replace(" ", "&"); */
-        axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userQuery}&limit=25&offset=0&rating=G&lang=en`)
+        axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${userQuery}&limit=5&offset=0&rating=G&lang=en`)
         .then(data => {
-            /* data.request.response */
-            console.log(data.request.response)
+            const parsedGIPHYData = JSON.parse(data.request.responseText)
+            for (let i=0; i < 5; i++){
+                console.log(parsedGIPHYData.data[i].images.fixed_height_small.url)
+                $(`#gif${i+1}`).prop("src", parsedGIPHYData.data[i].images.fixed_height_small.url)
+            }
+            /* console.log(parsedGIPHYData) */
         })
     }
 })
